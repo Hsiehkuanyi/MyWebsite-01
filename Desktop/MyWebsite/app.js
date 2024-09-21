4a51ef4b5d53a3b1c92f5ecc2cbc2d16d2b9f789
Hsiehkuanyi <hsiehkaunyi891102@gmail.com>
2024-09-21 17:11:09 +0800

    Initial commit - 分享書籍、電影和好物網站

--- Desktop/MyWebsite/app.js
+++ Desktop/MyWebsite/app.js
@@ -1,3 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("Website is ready!");
+document.getElementById("searchButton").addEventListener("click", function() {
+    let query = document.getElementById("searchInput").value;
+    alert("搜尋結果：" + query);
 });
